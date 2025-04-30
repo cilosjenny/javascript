3296b13d4f597a6b1e41257402de9a9911d80401
jenny <jenniferbarbosa4439@gmail.com>
2025-04-30 14:15:43 -0300

    dever de prompt

---
+++ deverpom.js
@@ -0,0 +1,22 @@
+// crie um prompt e peça ao usuário q digite uma nota.
+// se a nota for maior q ou igual a 7, imprima "aprovado!"
+//se a nota for igual a 5 ou igual a 6, imprima "Recuperação!"
+//se a nota for menor q 5, imprima "reprovado!"
+
+
+let nota =  Number (prompt("Digite sua nota"))
+
+
+if(nota >=7){
+    console.log("Aprovado!");
+    
+}else if(nota ===5 || nota ===6 ){
+    console.log("Recuperação!");
+    
+}else{
+    console.log("Reprovado!");
+    
+}
+
+
+//"===": significa extritamente igual
---
+++ prompt.js
@@ -0,0 +1,20 @@
+// Crie um prompt e peça ao usuário que digite seu nome.
+//Imprima o resultado no console com "Seu nome é..."
+
+let nome = prompt("Digite seu nome:")
+
+console.log("Seu nome é:" + nome);
+
+//template string: usando `` em vez de "" para usar uma string
+//deixa inserir js na string
+//EX:
+
+console.log(`seu nome é${nome}`);
+
+//ELE TBM DEIXA QUEBRAR A LINHA
+//EX:
+
+console.log(`Seu nome é 
+    ${nome}`);
+
+
